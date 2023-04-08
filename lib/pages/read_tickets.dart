198001681ef0bbcc9c5ab3e1d5350757f2bb814f
andres <andresc.gomez@upb.edu.co>
2023-04-07 23:41:00 -0500

    Update read_tickets.dart

--- lib/pages/read_tickets.dart
+++ lib/pages/read_tickets.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:qvent/services/firebase_services.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
@@ -15,12 +17,23 @@ class ReadTicketsPage extends StatefulWidget {
 
 class _ReadTicketsPageState extends State<ReadTicketsPage> {
   final qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? barcode;
   QRViewController? controller;
 
   @override
   void dispose() {
     controller?.dispose();
     super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    super.reassemble();
+
+    if (Platform.isAndroid) {
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
   }
 
   @override
@@ -33,18 +46,42 @@ class _ReadTicketsPageState extends State<ReadTicketsPage> {
         alignment: Alignment.center,
         children: <Widget>[
           buildQrView(context),
+          Positioned(bottom: 10, child: buildResult()),
         ],
       ),
     ),
   );
 
+  Widget buildResult() => Container(
+    padding: EdgeInsets.all(12),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color:Colors.white24,
+    ),
+    child: Text(
+    barcode != null ? 'Result : ${barcode!.code}' : 'Scan a code!',
+    maxLines:3,
+    ),
+    
+    );
+
   Widget buildQrView(BuildContext context) => QRView(
     key: qrKey,
     onQRViewCreated: onQRViewCreated,
+    overlay: QrScannerOverlayShape(
+      borderColor: Colors.white,
+      borderRadius: 10,
+      borderLength: 20,
+      borderWidth: 10,
+      cutOutSize: MediaQuery.of(context).size.width * 0.8,
+    ),
   );
 
   void onQRViewCreated(QRViewController controller){
     setState(() => this.controller = controller);
+
+    controller.scannedDataStream
+      .listen((barcode) => setState(() => this.barcode = barcode));
 
   }
 }
