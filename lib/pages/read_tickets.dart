57d4022d8f39e82054311916dc195a50a2eb9172
Samir Colpas <samir.colpas@hotmail.com>
2023-05-02 14:27:02 -0500

    remove read tickets from tickets list

--- lib/pages/read_tickets.dart
+++ lib/pages/read_tickets.dart
@@ -5,8 +5,6 @@ import 'package:qr_code_scanner/qr_code_scanner.dart';
 //Sizer tool
 import 'package:sizer/sizer.dart';
 
-
-
 class ReadTicketsPage extends StatefulWidget {
   const ReadTicketsPage({super.key});
 
@@ -18,7 +16,10 @@ class _ReadTicketsPageState extends State<ReadTicketsPage> {
   final qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? barcode;
   QRViewController? controller;
-
+  dynamic tickets;
+  dynamic eventId;
+  dynamic read_tickets;
+  late bool ticketExists;
   @override
   void dispose() {
     controller?.dispose();
@@ -38,72 +39,74 @@ class _ReadTicketsPageState extends State<ReadTicketsPage> {
   @override
   Widget build(BuildContext context) {
     final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
-    dynamic tickets = arguments['tickets'];
-    dynamic read_tickets= arguments['read_tickets'];
-    dynamic eventId = arguments['eventId'];
+    dynamic tickets_argument = arguments['tickets'];
+    dynamic read_tickets_argument = arguments['read_tickets'];
+    dynamic eventId_argument = arguments['eventId'];
+    tickets = tickets_argument;
+    read_tickets = read_tickets_argument;
+    eventId = eventId_argument;
     var safeArea = SafeArea(
-    child: 
-    Scaffold(
-      appBar: AppBar(
-        title: const Text('Generar boletas'),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Generar boletas'),
+        ),
+        body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            buildQrView(context),
+            Positioned(bottom: 10, child: buildResult()),
+          ],
+        ),
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          buildQrView(context),
-          Positioned(bottom: 10, child: buildResult()),
-        ],
-      ),
-    ),
-  );
+    );
     return safeArea;
   }
 
   Widget buildResult() => Container(
-    padding: const EdgeInsets.all(12),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(8),
-      color:Colors.white24,
-    ),
-    child: Text(
-    barcode != null ? 'Result : ${barcode!.code}' : 'Scan a code!',
-    maxLines:3,
-    ),
-    
-    );
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white24,
+        ),
+        child: Text(
+          barcode != null ? 'Result : ${barcode!.code}' : 'Scan a code!',
+          maxLines: 3,
+        ),
+      );
 
   Widget buildQrView(BuildContext context) => QRView(
-    key: qrKey,
-    onQRViewCreated: onQRViewCreated,
-    overlay: QrScannerOverlayShape(
-      borderColor: Colors.white,
-      borderRadius: 10,
-      borderLength: 20,
-      borderWidth: 10,
-      cutOutSize: MediaQuery.of(context).size.width * 0.8,
-    ),
-  );
+        key: qrKey,
+        onQRViewCreated: onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderColor: Colors.white,
+          borderRadius: 10,
+          borderLength: 20,
+          borderWidth: 10,
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+        ),
+      );
 
-  void onQRViewCreated(QRViewController controller){
+  void onQRViewCreated(QRViewController controller) {
     setState(() => this.controller = controller);
 
-    controller.scannedDataStream
-      .listen((barcode) => setState(() => this.barcode = barcode));
-
+    controller.scannedDataStream.listen(
+      (barcode) => setState(() {
+        this.barcode = barcode;
+        ticketExists = findTicket(barcode.code, tickets);
+        deleteTicket(barcode.code, tickets);
+        updateEventTickets(eventId, tickets);
+      }),
+    );
   }
 
-bool findTicket(String ticketNumber, dynamic tickets) {
-  List<dynamic> listTickets = tickets;
-  return listTickets.contains(ticketNumber);
+  bool findTicket(String? ticketNumber, dynamic tickets) {
+    List<dynamic> listTickets = tickets;
+    return listTickets.contains(ticketNumber);
+  }
+
+  List<dynamic> deleteTicket(String? ticketNumber, dynamic tickets) {
+    List<dynamic> updatedTickets = tickets;
+    updatedTickets.remove(ticketNumber);
+    return updatedTickets;
+  }
 }
-
-List<dynamic> deleteTicket(String ticketNumber, dynamic tickets) {
-  List<dynamic> updatedTickets = tickets;
-  updatedTickets.remove(ticketNumber);
-  return updatedTickets;
-}
-
-}
-
-
-
